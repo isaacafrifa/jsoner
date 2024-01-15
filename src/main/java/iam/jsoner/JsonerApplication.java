0e426e97a64efa55fe00c950432bffb03f2923a5
Afrifa <isaac.afrifa3@yahoo.com>
2024-01-15 17:08:12 +0200

    initial commit

---
+++ src/main/java/iam/jsoner/JsonerApplication.java
@@ -0,0 +1,13 @@
+package iam.jsoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonerApplication.class, args);
+    }
+
+}
